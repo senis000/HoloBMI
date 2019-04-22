00804a430e415caffdc0d657bdf9ec0c9b480d2d
senis000 <nuria.vendrell.llopis@gmail.com>
2019-04-21 21:19:08 -0400

    random points-index
    Create XML now generates a randomize index for activation of neurons

---
+++ createXmlFile.m
@@ -0,0 +1,55 @@
+function createXmlFile(savePath, numberNeurons, reps)
+%{
+Function to create a gpl file to be uploaded to the prairie view
+savePAth --> path where to save the .gpl file
+numberNeurons -> How many neurons to randomly activate
+reps --> amount of stim per neuron
+%}
+
+    %% Parameters of the GUI
+    Iter = 1;
+    IterDelay = 1000.00;
+    UncagingLaser = "fidelityHP";
+    UncagingLaserPower = 0.4;
+    InitialDelay = 0.22;
+    InterPointDelay = 4800;
+    Duration = 200;
+    SpiralRevolutions = 5; 
+    Repetitions = 1;  
+    AllPointsAtOnce = "False";
+    
+    %% Array of random holostims 
+    holovector = nan(numberNeurons,reps);
+    for rr = 1:reps
+        holovector(:,rr) = randsample(numberNeurons,numberNeurons);
+    end
+    holovector = holovector(:);
+    
+    
+    %% prepare file
+
+    % print the first part of the text
+    fileID = fopen([savePath, 'holoMask.xml'],'wt');
+    fprintf(fileID,'<?xml version="1.0" encoding="utf-8"?>\n');
+    fprintf(fileID,'<PVSavedMarkPointSeriesElements Iterations="%d" IterationDelay="%.2f">\n', Iter, IterDelay);
+    
+    % print for each point
+    formatSpec1 = ['  <PVMarkPointElement Repetitions="%d" UncagingLaser="%s" UncagingLaserPower="%.1f"', ...
+        ' TriggerFrequency="None" TriggerSelection="None" TriggerCount="1"', ...
+        ' AsyncSyncFrequency="None" VoltageOutputCategoryName="None" VoltageRecCategoryName="None"', ...
+        ' parameterSet="CurrentSettings">\n'];
+    formatSpec2 = ['    <PVGalvoPointElement InitialDelay="%.2f"', ...
+        ' InterPointDelay="%d" Duration="%d" SpiralRevolutions="%d" AllPointsAtOnce="%s"', ...
+        ' Points="Point %d" Indices="%d" />\n'];
+    
+    for ind = 1:numberNeurons*reps
+        %TODO change from pixelspace to motorspace
+        content1 = [InitialDelay, InterPointDelay, Duration, SpiralRevolutions]; 
+        content2 = [holovector(ind), ind];
+        fprintf(fileID, formatSpec1, Repetitions, UncagingLaser, UncagingLaserPower);
+        fprintf(fileID, formatSpec2, content1, AllPointsAtOnce, content2);
+        fprintf(fileID, '  </PVMarkPointElement>\n');
+    end
+    fprintf(fileID,'</PVSavedMarkPointSeriesElements>\n');
+    fclose(fileID);
+end
