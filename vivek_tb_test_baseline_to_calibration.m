4563384d4adc285961d4cfc3a485fe56ae441be4
senis000 <nuria.vendrell.llopis@gmail.com>
2019-05-02 17:07:48 -0400

    notebook added

--- vivek_tb_test_baseline_to_calibration.m
+++ vivek_tb_test_baseline_to_calibration.m
@@ -15,20 +15,18 @@ exist(base_file)
 %Need to streamline getting the files from 'BaselineAcqnvsPrairie.m' to 
 %baseline2target.m
 
-frame_rate = 30;
-
 n_f_file = base_file;
 exist(n_f_file)
 Acomp_file = fullfile(savePath, 'redcomp.mat');
 exist(Acomp_file)
 E1_base = [27 23];
 E2_base = [21 8]; %Needs to have chrome!
-frames_per_reward_range = [1.5 0.1]*60*frame_rate %[1 1.5]*60*frame_rate
+frames_per_reward_range = [1.5 0.1]*60*frameRate %[1 1.5]*60*frameRate
 
 target_on_cov_bool = 0
 prefix_win = 40
 f0_win_bool = 1
-f0_win = 10 %2*60*frame_rate
+f0_win = 10 %2*60*frameRate
 dff_win_bool = 1
 dff_win = 2 %frames
  
