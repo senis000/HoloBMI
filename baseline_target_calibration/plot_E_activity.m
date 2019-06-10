b9513fcd9569b02cab1467ed2303095c15372347
Vivek Athalye <vivek.athalye89@gmail.com>
2019-06-10 10:24:24 -0400

    include t for plot_E_activity

--- baseline_target_calibration/plot_E_activity.m
+++ baseline_target_calibration/plot_E_activity.m
@@ -1,4 +1,4 @@
-function [h, offset_vec] = plot_E_activity(n, E_id, E_color, offset)
+function [h, offset_vec] = plot_E_activity(t,n, E_id, E_color, offset)
 %4.18.19
 %n - neural activity, num_samples x num_neurons
 %E_id - vector of 1's and 2's, indicating membership to E1 or E2
@@ -18,5 +18,5 @@ for i=1:num_neurons
         offset_vec = [offset_vec offset]; 
     end
     y_plot = y_plot-offset;
-    plot(y_plot, 'Color', E_color{E_id(i)}); 
+    plot(t,y_plot, 'Color', E_color{E_id(i)}); 
 end
