47230a160ed0b110cd18d2e4d97f55cada832b91
Vivek Athalye <vivek.athalye89@gmail.com>
2019-09-28 10:09:00 -0400

    dff2cursor_target BUG fixed!
    BUG:
    (I used indexes from vector of just E2 neurons (E2_dom_sel) and applied to full neural vector (dff)
    [E2_dom_samples, E2_dom_sel]    = max(E2, [], 2); %E2_dom  CAREFUL THIS MAY BRING TWO!!
    % E2_dom_samples = dff(E2_dom_sel(1));

--- baseline_target_calibration/baseline2target_vE1strict.m
+++ baseline_target_calibration/baseline2target_vE1strict.m
@@ -140,7 +140,7 @@ mkdir(plotPath);
 load(n_f_file); 
 f_base = baseActivity; 
 f_base(:,isnan(f_base(1,:))) = [];
-f_base = f_base.';
+f_base = f_base.'; %num_samples X num_neurons
 
 % f_base = f_base(1:1000, :); %debugging input data with nans... 
 %Assume variable is called f_base
@@ -385,10 +385,10 @@ else
     n_analyze = dff;
 end
  
-valid_idxs = find(~isnan(n_analyze(:,1)));
-n_analyze = n_analyze(valid_idxs, :); 
+valid_idxs  = find(~isnan(n_analyze(:,1)));
+n_analyze   = n_analyze(valid_idxs, :); 
 analyze_cov = cov(n_analyze);
-analyze_mean = nanmean(n_analyze); 
+analyze_mean = nanmean(n_analyze); %takes mean along dim 1.  n_analyze is num_samples X num_neurons.
 if(plot_cov_bool)
     h = figure;
     imagesc(analyze_cov); 
@@ -423,19 +423,19 @@ cursor_cov = decoder'*analyze_cov*decoder;
 %Inputs: 
 %frames_per_reward_range
 %cov_bool
-reward_per_frame_range = 1./frames_per_reward_range;
-E1_mean = mean(analyze_mean(E1_sel));
-E1_std = sqrt((E1_sel/num_E1)'*analyze_cov*(E1_sel/num_E1));
-E2_subord_mean = zeros(num_E2,1);
-E2_subord_std = zeros(num_E2,1); 
-E1_analyze = n_analyze(:,E1_sel); 
-E2_analyze = n_analyze(:,E2_sel); 
+reward_per_frame_range  = 1./frames_per_reward_range;
+E1_mean                 = mean(analyze_mean(E1_sel));
+E1_std                  = sqrt((E1_sel/num_E1)'*analyze_cov*(E1_sel/num_E1));
+E2_subord_mean          = zeros(num_E2,1);
+E2_subord_std           = zeros(num_E2,1); 
+E1_analyze              = n_analyze(:,E1_sel); 
+E2_analyze              = n_analyze(:,E2_sel); 
 for E2_i = 1:num_E2
-    subord_sel = E2_sel;
-    subord_sel(E2_sel_idxs(E2_i)) = 0; 
-    E2_subord_mean(E2_i) = mean(analyze_mean(subord_sel));     
-    var_i = subord_sel'*analyze_cov*subord_sel; 
-    E2_subord_std(E2_i) = sqrt(var_i);     
+    subord_sel                      = E2_sel;
+    subord_sel(E2_sel_idxs(E2_i))   = 0; 
+    E2_subord_mean(E2_i)            = mean(analyze_mean(subord_sel));     
+    var_i                           = subord_sel'*analyze_cov*subord_sel; 
+    E2_subord_std(E2_i)             = sqrt(var_i);     
 end
 
 E2_sum_analyze = sum(E2_analyze,2); 
@@ -458,20 +458,20 @@ E2_subord_mean_analyze          = (E2_sum_analyze - E2_dom_samples)/(num_E2-1);
 %neural activity
 
 %T:
-T0 = max(cursor_obs);
-T = T0; 
-T_min = 0;
+T0                  = max(cursor_obs);
+T                   = T0; 
+T_min               = 0;
 
 %E2:
-E2_coeff0 = 0.5;
-E2_coeff = E2_coeff0; %multiples the std dev, for figuring out E2_subord_thresh
-E2_coeff_min = 0.3; 
-E2_subord_thresh = E2_subord_mean+E2_subord_std*E2_coeff;
+E2_coeff0           = 0.5;
+E2_coeff            = E2_coeff0; %multiplies the std dev, for figuring out E2_subord_thresh
+E2_coeff_min        = 0.3; 
+E2_subord_thresh    = E2_subord_mean+E2_subord_std*E2_coeff;
 
 %E1:
-E1_coeff0 = 0;
-E1_coeff = E1_coeff0;
-E1_thresh = E1_mean + E1_coeff*E1_std; %E1_mean_max; %E1_mean;
+E1_coeff0           = 0;
+E1_coeff            = E1_coeff0;
+E1_thresh           = E1_mean + E1_coeff*E1_std; %E1_mean_max; %E1_mean;
 
 
 T_delta = 0.05;
